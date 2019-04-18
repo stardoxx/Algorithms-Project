1fba76f23f9006d3f92d491bebd763664de3e31e
Saransh <saranshjain161@gmail.com>
2019-04-18 19:19:54 +0530

    Create graph and fix error

--- Algo_Project/DeliverySys.cpp
+++ Algo_Project/DeliverySys.cpp
@@ -1,26 +1,91 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ipair pair<int,int>
 
 class city; //class for internal parts of a city
 class country;  // class for connecting cities across country
 
 class graph{
         int vertices;
-        int ** adj;
+        //int ** adj;
+        list<ipair> * adjlist;
         map<string,int> indexMap;
         map<int,string> nameMap;
     public:
         friend class city;
         friend class country;
+        graph()
+        {
+
+        }
         graph(int v){
             this->vertices = v;
-            adj = new int *[v];
-            for(int i =0;i<v;i++){
-                adj[i] = new int [v];
-                memset(adj[i], 0, v * sizeof(int));
+            adjlist = new list < ipair >[v];
+            //adj = new int *[v];
+            //for(int i =0;i<v;i++){
+            //    adj[i] = new int [v];
+            //    memset(adj[i], 0, v * sizeof(int));
+            //}
+        }
+        void constructorhelper(int v)
+        {
+            this->vertices = v;
+            adjlist = new list < ipair >[v];
+            //adj = new int *[v];
+            //for(int i =0;i<v;i++){
+            //    adj[i] = new int [v];
+            //    memset(adj[i], 0, v * sizeof(int));
+            //}
+        }
+        void addAllCityName()
+        {
+            int i=0;
+            string placeName = "zero";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=1;placeName="first";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=2;placeName="second";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=3;placeName="third";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=4;placeName="fourth";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=5;placeName="fifth";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=6;placeName="sixth";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=7;placeName="seventh";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+            i=8;placeName="eight";
+            indexMap.insert(make_pair(placeName,i));//mapping places with index
+            nameMap.insert(make_pair(i,placeName));
+        }
+        void addEdge(int u,int v,int weight)
+        {
+            adjlist[u].push_back(make_pair(weight,v));
+            adjlist[v].push_back(make_pair(weight,u));
+        }
+        void printGraph()
+        {
+            list<ipair>::iterator it;
+            for(int i=0;i<vertices;i++)
+            {
+                cout<<nameMap[i]<<" ";
+                for(it = adjlist[i].begin();it!=adjlist[i].end();++it)
+                {
+                    cout<<i<<" "<<it->second<<endl;
+                }
             }
         }
-        void formNetwork(){
+        /*void formNetwork(){
             for(int i =0;i<vertices;i++){
                 cout<<"enter name of place: "<<endl;
                 string placeName;
@@ -45,7 +110,7 @@ class graph{
                     }
                 }
             }
-        }
+        }*/
         // function for taking paths commented so that you use according to your use case
         /*
             void showPath(int path[][v],int u,int w){
@@ -58,7 +123,7 @@ class graph{
         */
 
         //function for floyd Warshall
-        void floydWarshall(){
+        /*void floydWarshall(){
             int dist[vertices][vertices], i, j, k;//distance matrix
             int path[vertices][vertices];         //path matrix for path finding
             for (i = 0; i < vertices; i++)
@@ -93,7 +158,7 @@ class graph{
                         }
                     }
                 }
-            }
+            }*/
             /*
                 for extracting paths
             for (int i = 0; i < v; i++)
@@ -107,32 +172,62 @@ class graph{
                     }
                 }
             }*/
-        }
+        //}
 };
 // class for intracity network
 class city{
         string name;
         graph * g;
         int places;
-        
+
     public:
         friend class graph;
         friend class country;
-        city(string name,int places){
+        void addNameAndPlaces(string name,int places){
+            g = new graph;
             this->name = name;
             this->places = places;
-            g(places);
-            g->formNetwork();
+            g->constructorhelper(places);
+//            g->formNetwork();
+        }
+        city(string name,int places){
+            g = new graph;
+            this->name = name;
+            this->places = places;
+            g->constructorhelper(places);
+ //           g->formNetwork();
         }
 
-
+        city(){}
+        void addAllCityName()
+        {
+            g->addAllCityName();
+        }
+        void addEdge(int u,int v,int weight)
+        {
+            g->addEdge(u,v,weight);
+        }
+        void printGraph(){
+            g->printGraph();
+        }
 
 };
 
-
-
 int main(){
-    
-
+    city *c1;
+    c1 = new city;
+    c1->addNameAndPlaces("abc",9);
+    c1->addAllCityName();
+    c1->addEdge(0,1,1);
+     c1->addEdge(1,2,4);
+    c1->addEdge(1,5,3);
+    c1->addEdge(2,4,1);
+    c1->addEdge(2,3,2);
+    c1->addEdge(3,8,1);
+    c1->addEdge(7,8,3);
+    c1->addEdge(6,8,5);
+    c1->addEdge(0,6,2);
+    c1->addEdge(6,7,2);
+    c1->printGraph();
     return 0;
 }
