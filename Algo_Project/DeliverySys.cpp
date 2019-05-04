53de098f95bdcf406b4aa7dc5877a514508917ff
Saransh <saranshjain161@gmail.com>
2019-05-04 20:32:51 +0530

    TSP and pathfinder added

--- Algo_Project/DeliverySys.cpp
+++ Algo_Project/DeliverySys.cpp
@@ -12,6 +12,13 @@ class graph{
         map<int,string> nameMap;
         int mini;
         int *minipath;
+        int **adjmatrix;
+        int **dist;
+        int **path;
+        int lastvertex;
+        int *finalpathforTSP;
+        int final_res;
+        int * visitedforTSP;
     public:
         friend class city;
         friend class country;
@@ -31,14 +38,21 @@ class graph{
                 return true;
         }
 
+        /*
+			By Backtracking find all the path
+			when all mustvisit nodes are visit compare it with all previous computed path if less then save
+        */
+
         void startthegame(int parent,bool visit[],int path[],vector<int>mustvisit,int cost)
         {
         	visit[parent] = true;
         	if(isAllVisited(visit,mustvisit) == true)
         	{
-        		if(cost<mini)
+        		if(dist[parent][0] + cost<mini)
         		{
-        			mini = cost;
+        			mini = cost+dist[parent][0];
+
+        			lastvertex = parent;
         			for(int i=0;i<vertices;i++)
         				minipath[i] = path[i];
         		}
@@ -56,9 +70,13 @@ class graph{
         	visit[parent] = false;
         	return;
         }
+        /*
+			following function helps in initiate the startthegamefunction
+        */
 
         void pathfinder()
         {
+        	FloydWarshell();
         	mini = INT_MAX;
         	vector<int>mustvisit;
         	mustvisit.push_back(2);mustvisit.push_back(6);mustvisit.push_back(7);mustvisit.push_back(1);
@@ -66,7 +84,7 @@ class graph{
         	int path[vertices];
         	for(int i=0;i<vertices;i++)
         		path[i] = -1;
-        	bool startingNode = 0;
+        	int startingNode = 0;
         	path[startingNode] = -2;
         	visit[startingNode] = true;
         	for(auto it = adjlist[startingNode].begin();it!=adjlist[startingNode].end();++it)
@@ -76,9 +94,23 @@ class graph{
         		path[it->first] = -1;
         	}
         	cout<<endl<<endl;
-        	cout<<mini<<endl<<endl;
-        	for(int i=0;i<vertices;i++)
-        		cout<<i<<" "<<minipath[i]<<"  "<<endl;
+        	//cout<<"minimum => "<<mini<<endl<<endl;
+        	int temp = lastvertex;
+        	stack<int>printingpath;
+        	while(temp != -2)
+        	{
+        		printingpath.push(temp);
+        		temp = minipath[temp];
+        	}
+        	while(!printingpath.empty())
+        	{
+        		cout<<nameMap[printingpath.top()]<<" >> ";
+        		printingpath.pop();
+        	}
+        	cout<<endl<<"Delivery Done"<<endl<<"Return Back"<<endl;
+        	cout<<nameMap[lastvertex]<<" >> ";
+        	printPath(lastvertex,0);
+        	cout<<nameMap[0]<<endl;
         }
 
         
@@ -89,6 +121,23 @@ class graph{
             adjlist = new list < ipair >[v];
             mini = INT_MAX;
             minipath = new int [v];
+            adjmatrix = new int * [v];
+            dist = new int *[v];
+            path = new int *[v];
+            final_res = INT_MAX;
+         	finalpathforTSP = new int [v+1];  
+         	visitedforTSP = new int[v];
+            for(int i=0;i<v;i++)
+            {
+            	adjmatrix[i] = new int[v];
+            	dist[i] = new int [v];
+            	path[i] = new int [v];
+            }
+         	for(int i=0;i<v;i++)
+         	{
+         		for(int j=0;j<v;j++)
+         			adjmatrix[i][j] = INT_MAX;
+         	}
         }
         void constructorhelper(int v)
         {
@@ -96,11 +145,30 @@ class graph{
             adjlist = new list < ipair >[v];
             mini = INT_MAX;
             minipath = new int [v];
+            adjmatrix = new int * [v];
+            dist = new int *[v];
+            path = new int *[v];
+            final_res = INT_MAX;
+            finalpathforTSP = new int [v+1];
+            visitedforTSP = new int[v];
+            for(int i=0;i<v;i++)
+            {
+            	adjmatrix[i] = new int[v];
+            	dist[i] = new int [v];
+            	path[i] = new int [v];
+            }
+         	for(int i=0;i<v;i++)
+         	{
+         		for(int j=0;j<v;j++)
+         			adjmatrix[i][j] = INT_MAX;
+         	}
+         	 
         }
         void addEdge(int u,int v,int weight)
         {
             adjlist[u].push_back(make_pair(v,weight));
             adjlist[v].push_back(make_pair(u,weight));
+            adjmatrix[u][v] = adjmatrix[v][u] = weight;
         }
 
         void addAllCityName()
@@ -148,68 +216,236 @@ class graph{
             }
         }
         // function for taking paths commented so that you use according to your use case
-        /*
-            void showPath(int path[][v],int u,int w){
-                if(path[u][w] == u){
-                return ;
-              }
-                showPath(path,u,path[u][w]);
-                cout<<path[u][w]<<" ";
-            }
-        */
+        
+        void printPath(int v, int u)
+		{
+			if (path[v][u] == v)
+				return;
 
-        //function for floyd Warshall
-        /*void floydWarshall(){
-            int dist[vertices][vertices], i, j, k;//distance matrix
-            int path[vertices][vertices];         //path matrix for path finding
-            for (i = 0; i < vertices; i++)
-            {
-                for (j = 0; j < vertices; j++)
-                {
-                    dist[i][j] = adj[i][j];
-                    if (i == j)
-                    {
-                        path[i][j] = 0;
-                    }
-                    else if (dist[i][j] != INT_MAX)
-                    {
-                        path[i][j] = i;
-                    }
-                    else
-                    {
-                        path[i][j] = -1;
-                    }
-                }
-            }
-            for (k = 0; k < vertices; k++)
-            {
-                for (i = 0; i < vertices; i++)
-                {
-                    for (j = 0; j < vertices; j++)
-                    {
-                        if (dist[i][k] + dist[k][j] < dist[i][j])
-                        {
-                            dist[i][j] = dist[i][k] + dist[k][j];
-                            path[i][j] = path[k][j];
-                        }
-                    }
-                }
-            }*/
-            /*
-                for extracting paths
-            for (int i = 0; i < v; i++)
-            {
-                for (int j = 0; j < v; j++)
-                {
-                    if (i != j && path[i][j] != -1)
-                    {
-                        showPath(path, i, j);
-                        cout << endl;
-                    }
-                }
-            }*/
-        //}
+			printPath(v, path[v][u]);
+			cout << nameMap[path[v][u]] << " >> ";
+		}
+        
+        void FloydWarshell()
+		{
+			for (int v = 0; v < vertices; v++) 
+			{
+				for (int u = 0; u < vertices; u++) 
+				{
+					// initally cost would be same as weight 
+					// of the edge
+					dist[v][u] = adjmatrix[v][u];
+
+					if (v == u)
+						path[v][u] = 0;
+					else if (dist[v][u] != INT_MAX)
+						path[v][u] = v;
+					else
+						path[v][u] = -1;
+				}
+			}
+			
+			// run Floyd-Warshell
+			for (int k = 0; k < vertices; k++) 
+			{
+				for (int v = 0; v < vertices; v++) 
+				{
+					for (int u = 0; u < vertices; u++) 
+					{
+						// If vertex k is on the shortest path from v to u,
+						// then update the value of dist[v][u], path[v][u]
+
+						if (dist[v][k] != INT_MAX && dist[k][u] != INT_MAX
+							&& dist[v][k] + dist[k][u] < dist[v][u]) 
+						{
+							dist[v][u] = dist[v][k] + dist[k][u];
+							path[v][u] = path[k][u];
+						}
+					}
+
+					// if diagonal elements become negative, the
+					// graph contains a negative weight cycle
+					if (dist[v][v] < 0) 
+					{
+						cout << "Negative Weight Cycle Found!!";
+						return;
+					}
+				}
+			}
+
+			// Print the shortest path between all pairs of vertices
+			
+		}
+
+
+ 
+		void copyToFinal(int curr_path[]) 
+		{ 
+		    for (int i=0; i<vertices; i++) 
+		        finalpathforTSP[i] = curr_path[i]; 
+		    finalpathforTSP[vertices] = curr_path[0]; 
+		} 
+		  
+		 
+		int firstMin( int i) 
+		{ 
+		    int min = INT_MAX; 
+		    for (int k=0; k<vertices; k++) 
+		        if (adjmatrix[i][k]<min && i != k) 
+		            min = adjmatrix[i][k]; 
+		    return min; 
+		} 
+		  
+
+		int secondMin( int i) 
+		{ 
+		    int first = INT_MAX, second = INT_MAX; 
+		    for (int j=0; j<vertices; j++) 
+		    { 
+		        if (i == j) 
+		            continue; 
+		  
+		        if (adjmatrix[i][j] <= first) 
+		        { 
+		            second = first; 
+		            first = adjmatrix[i][j]; 
+		        } 
+		        else if (adjmatrix[i][j] <= second && 
+		                 adjmatrix[i][j] != first) 
+		            second = adjmatrix[i][j]; 
+		    } 
+		    return second; 
+		} 
+		  
+		 
+		void TSPRec(int curr_bound, int curr_weight, 
+		            int level, int curr_path[]) 
+		{ 
+		     
+		    if (level==vertices) 
+		    { 
+		     
+		        if (adjmatrix[curr_path[level-1]][curr_path[0]] != 0) 
+		        { 
+		     
+		            int curr_res = curr_weight + 
+		                    adjmatrix[curr_path[level-1]][curr_path[0]]; 
+		  
+		     
+		            if (curr_res < final_res) 
+		            { 
+		                copyToFinal(curr_path); 
+		                final_res = curr_res; 
+		            } 
+		        } 
+		        return; 
+		    } 
+		  
+		     
+		    for (int i=0; i<vertices; i++) 
+		    { 
+		      
+		        if (adjmatrix[curr_path[level-1]][i] != 0 && 
+		            visitedforTSP[i] == false) 
+		        { 
+		            int temp = curr_bound; 
+		            curr_weight += adjmatrix[curr_path[level-1]][i]; 
+		  
+		      
+		            if (level==1) 
+		              curr_bound -= ((firstMin(curr_path[level-1]) + firstMin(i))/2); 
+		            else
+		              curr_bound -= ((secondMin(curr_path[level-1]) + firstMin( i))/2); 
+		  
+		      
+		            if (curr_bound + curr_weight < final_res) 
+		            { 
+		                curr_path[level] = i; 
+		                visitedforTSP[i] = true; 
+		  
+		     
+		                TSPRec(curr_bound, curr_weight, level+1, 
+		                       curr_path); 
+		            } 
+		  
+		      
+		            curr_weight -= adjmatrix[curr_path[level-1]][i]; 
+		            curr_bound = temp; 
+		  
+		      
+		            memset(visitedforTSP, false, sizeof(visitedforTSP)); 
+		            for (int j=0; j<=level-1; j++) 
+		                visitedforTSP[curr_path[j]] = true; 
+		        } 
+		    } 
+		}  
+		void TSP() 
+		{ 
+		    int curr_path[vertices+1]; 
+		    int curr_bound = 0; 
+		    memset(curr_path, -1, sizeof(curr_path)); 
+		    memset(visitedforTSP, 0, sizeof(curr_path)); 
+		  
+
+		    for (int i=0; i<vertices; i++) 
+		        curr_bound += (firstMin(i) + 
+		                       secondMin(i)); 
+		  
+
+		    curr_bound = (curr_bound&1)? curr_bound/2 + 1 : 
+		                                 curr_bound/2; 
+		  
+		 
+		    visitedforTSP[0] = true; 
+		    curr_path[0] = 0; 
+		  
+		 
+		    TSPRec(curr_bound, 0, 1, curr_path); 
+		} 
+        
 };
+
+class country{
+        string name;
+        graph * g;
+        int places;
+
+    public:
+        friend class graph;
+        void addNameAndPlaces(string name,int places){
+            g = new graph;
+            this->name = name;
+            this->places = places;
+            g->constructorhelper(places);
+//            g->formNetwork();
+        }
+        country(string name,int places){
+            g = new graph;
+            this->name = name;
+            this->places = places;
+            g->constructorhelper(places);
+ //           g->formNetwork();
+        }
+
+        country(){}
+        void addAllCityName()
+        {
+            g->addAllCityName();
+        }
+        void addEdge(int u,int v,int weight)
+        {
+            g->addEdge(u,v,weight);
+        }
+        void printGraph(){
+            g->printGraph();
+            g->pathfinder();
+        }
+
+};
+
+
+
+
 // class for intracity network
 class city{
         string name;
